fd44a249ee87c62cd8ac78940af5a5feb411036c
anlei <404280116@qq.com>
2023-04-27 20:13:35 +0800

    demo

---
+++ 基于STL的演讲比赛流程管理系统/speechManager.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+//设计演讲比赛的管理类
+class SpeechManager
+{
+public:
+	//构造函数
+	SpeechManager();
+
+	//展示菜单
+	void showMenu();
+	
+	//析构函数
+	~SpeechManager();
+
+};
---
+++ 基于STL的演讲比赛流程管理系统/演讲比赛流程管理系统.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "speechManager.h"
+
+
+using namespace std;
+
+int main()
+{
+	//创建管理类对象
+	SpeechManager sm;
+
+	sm.showMenu();
+
+
+	system("pause");
+	return 0;
+}
